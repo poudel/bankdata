349df3a6fb75816014a72a420571f69b7699c73e
keshab <self@keshab.net>
2019-09-22 17:26:40 +0200

    add basic auth

--- programming/nest_api.py
+++ programming/nest_api.py
@@ -1,18 +1,36 @@
 from typing import List
-from fastapi import FastAPI, Query, Body
+from fastapi import FastAPI, Query, Body, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.status import HTTP_401_UNAUTHORIZED
+
 
 from nest_lib import nest_dicts
 
 
 app = FastAPI()
+security = HTTPBasic()
+
+USERNAME = "admin"
+PASSWORD = "admin"
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    if credentials.username != USERNAME or credentials.password != PASSWORD:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @app.post("/api/v1/nest_dicts")
 async def nest_dicts_api(
     input_data: List[dict] = Body(..., min_length=1),
     nesting_keys: List[str] = Query(..., min_length=1),
+    username: str = Depends(get_current_username),
 ):
     try:
         return nest_dicts(input_data, nesting_keys)
     except ValueError as err:
-        return {'error': str(err)}, 422
+        return {"error": str(err)}, 422
--- programming/tests/test_nest_api.py
+++ programming/tests/test_nest_api.py
@@ -1,10 +1,18 @@
+import base64
 import pytest
 from starlette.testclient import TestClient
-from nest_api import app
+from nest_api import app, USERNAME, PASSWORD
 from tests.utils import read_json_fixture, COMMON_TEST_PARAMS
 
 
 api_client = TestClient(app)
+
+
+def get_basicauth_headers():
+    credential = f"{USERNAME}:{PASSWORD}"
+    credential_b64 = base64.b64encode(credential.encode()).decode()
+    header = {"Authorization": f"Basic {credential_b64}"}
+    return header
 
 
 @pytest.mark.parametrize(*COMMON_TEST_PARAMS)
@@ -13,7 +21,10 @@ def test_nest_dicts_api(input_file, expected_output_file, nesting_keys):
     expected_response = read_json_fixture(expected_output_file)
 
     response = api_client.post(
-        "/api/v1/nest_dicts", params={"nesting_keys": nesting_keys}, json=input_data
+        "/api/v1/nest_dicts",
+        params={"nesting_keys": nesting_keys},
+        json=input_data,
+        headers=get_basicauth_headers(),
     )
     assert response.status_code == 200
     assert response.json() == expected_response
